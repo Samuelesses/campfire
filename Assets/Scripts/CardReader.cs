d975064cd65547235ba2e3221a2de4e168c78d1e
Crason02 <carsonexrodgers@gmail.com>
2026-02-21 11:35:46 -0800

    mian menu updates

--- Assets/Scripts/CardReader.cs
+++ Assets/Scripts/CardReader.cs
@@ -5,16 +5,24 @@ public class CardReader : MonoBehaviour
 {
     private string currentCardData;
     public Dictionary<string, CardPlayerData> cardDatabase = new Dictionary<string, CardPlayerData>();
+    public int hatListLength;
+    public CardPlayerData[] players = new CardPlayerData[4];
+    public int playersIndex = 0;
 
     public class CardPlayerData
     {
         public string name;
-        public string hexColor;
-
-        public CardPlayerData(string _name, string _hexColor)
+        public float color1;
+        public float color2;
+        public float color3;
+        public int hatIndex;
+        public CardPlayerData(string _name, float _color1, float _color2, float _color3, int _hatIndex)
         {
             name = _name;
-            hexColor = _hexColor;
+            _color1 = _color1;
+            _color2 = _color2;
+            _color3 = _color3;
+            _hatIndex = _hatIndex;
         }
     }
 
@@ -41,12 +49,14 @@ public class CardReader : MonoBehaviour
         {
             Debug.Log("CARD EXISTS IN DATABASE");
         }
-        else
+        else if(playersIndex<players.Length)
         {
             Debug.Log("CARD NOT FOUND IN DATABASE. ADDING");
-
-            cardDatabase.Add(currentCardData, new CardPlayerData("NAME HERE", "#000000"));
-            Debug.Log(cardDatabase[currentCardData].name + cardDatabase[currentCardData].hexColor);
+            CardPlayerData temp = new CardPlayerData("NAME HERE", Random.Range(0f,1f), Random.Range(0f,1f), Random.Range(0f,1f), Random.Range(0, hatListLength));
+            cardDatabase.Add(currentCardData, temp);
+            players[playersIndex] = temp;
+            playersIndex++;
+            Debug.Log(cardDatabase);
         }
 
         currentCardData = "";
